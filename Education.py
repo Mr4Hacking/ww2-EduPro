d6906e0e5025e4e7bc8d9ba6f6b41dd11f2c07a9
Mr4Hacking <78658975+Mr4Hacking@users.noreply.github.com>
2021-02-09 09:50:15 +0530

    Add files via upload

---
+++ Education.py
@@ -0,0 +1,33 @@
+print('\033[1;37m' " _____    _         ____")
+print("| ____|__| |_   _  |  _ \ _ __ ___")
+print("|  _| / _` | | | | | |_) |  __/ _ \ ")
+print("| |__| (_| | |_| | |  __/| | | (_)|")
+print("|_____\__,_|\__,_| |_|   |_|  \___/")
+
+print("")
+
+print('\033[1;33m' "1.world war I")
+
+print("")
+
+print('\033[1;34m' "3.world war II")
+
+print("")
+
+print('\033[1;35m' "2.world war III")
+
+print("")
+
+choice=int(input('\033[1;36m' "Enter Your Choice: >>  "))
+
+if (0<=choice<2):
+    print("")
+print('\033[1;32m' "1.Hitler s invasion of Poland in September 1939 marks the beginning of World War II.")
+
+print('\033[1;90m' "2. The invasion was so rapid that Hitler was able to conquer a large part of Poland within a month.")
+
+print('\033[1;32m' "3. Hitler s air strikes, tanks, submarines and infantry were very efficient.")
+
+print('\033[1;90m' "4.1914-1918 Same as the traditional method of World War I.")
+
+print('\033[1;32m' "5. The use of artillery drawn by horses in this battle by Polish war officers was nothing more than suicide in front of Hitler s tanks.")
